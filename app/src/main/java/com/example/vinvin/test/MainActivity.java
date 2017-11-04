b28d5f1d648305abf8dec8a9ae6192ea0635e920
Juho Sipola <jusipola@student.oulu.fi>
2017-11-04 22:01:01 +0200

    Upgraded functionality

--- app/src/main/java/com/example/vinvin/test/MainActivity.java
+++ app/src/main/java/com/example/vinvin/test/MainActivity.java
@@ -7,12 +7,26 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.nio.file.OpenOption;
+import java.util.ArrayList;
 import java.util.List;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     private WifiManager mainWifi;
     private TextView mainText;
     private static final int PERMISSION_TO_WIFI = 2;
+    private Menu menu;
 
     private BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
@@ -30,8 +45,8 @@ public class MainActivity extends AppCompatActivity {
             if (WifiManager.SCAN_RESULTS_AVAILABLE_ACTION.equalsIgnoreCase(intent.getAction())) {
 
                 networks = mainWifi.getScanResults();
-                for(int i = 0; i<networks.size(); i++){
-                    sb.append(Integer.toString(i+1) + ". ");
+                for (int i = 0; i < networks.size(); i++) {
+                    sb.append(Integer.toString(i + 1) + ". ");
                     sb.append("BSSID ");
                     sb.append(networks.get(i).BSSID + " Level " + networks.get(i).level + " ");
                     sb.append("SSID ");
@@ -51,42 +66,109 @@ public class MainActivity extends AppCompatActivity {
         ask();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        this.menu = menu;
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                Toast.makeText(getApplicationContext(), "cyka", LENGTH_LONG).show();
+                return true;
+            case R.id.settings_id:
+                SettingsView();
+                return true;
+            case R.id.turn_off:
+                try {
+                    unregisterReceiver(receiver);
+                    changeMenu(1);
+                } catch (IllegalArgumentException e) {
+                    createWifi();
+                    changeMenu(0);
+                }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void changeMenu(int i) {
+        MenuItem turn_status = menu.findItem(R.id.turn_off);
+        if (i == 1) {
+            turn_status.setTitle("Turn on");
+        } else {
+            turn_status.setTitle("Turn off");
+        }
+    }
+
+    public void CheckStatus(CheckBox box0, CheckBox box1, CheckBox box2) {
+
+
+    }
+
+    public void SettingsView() {
+        setContentView(R.layout.activity_settings);
+        Button exit_settings = (Button) findViewById(R.id.exit_button);
+        Button accept_button = (Button) findViewById(R.id.accept_button);
+        exit_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_main);
+            }
+        });
+        accept_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_main);
+            }
+        });
+    }
 
     private void createWifi() {
+        try {
 
-        mainWifi = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
-        mainWifi.setWifiEnabled(true);
+            mainWifi = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+            mainWifi.setWifiEnabled(true);
 
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
-        registerReceiver(receiver, filter);
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+            registerReceiver(receiver, filter);
 
-        mainWifi.startScan();
+            mainWifi.startScan();
+        } catch (NullPointerException e) {
+            Toast.makeText(getApplicationContext(), "Something went wrong!", LENGTH_LONG).show();
+            System.exit(1);
+        }
     }
-    private void ask(){
+
+    private void ask() {
         int Permission = ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION);
-        if(Permission != PackageManager.PERMISSION_GRANTED){
+        if (Permission != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
                     PERMISSION_TO_WIFI);
-        }else{
+        } else {
             createWifi();
         }
 
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode,
-            String permissions[], int[] grantResults) {
+                                           String permissions[], int[] grantResults) {
         switch (requestCode) {
             case PERMISSION_TO_WIFI: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     createWifi();
                 } else {
                     this.finish();
                     System.exit(0);
                 }
-                return;
             }
         }
     }
